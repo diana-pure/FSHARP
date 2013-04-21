37104d0a5b73fc75ceac28d0080d0434fcb54a60
DianaAnisutina <freilina_07@mail.ru>
2013-04-21 19:15:13 +0700

    HEART(!) can be drawn
    displaying amazing discrete plot

--- BitmapProject/BitmapProject/drawer.fs
+++ BitmapProject/BitmapProject/drawer.fs
@@ -3,14 +3,41 @@ open Microsoft.FSharp.Math
 open System
 open System.Drawing
 open System.Windows.Forms
-(*
+
 //crete scene and fill background color
-let plot = new Bitmap(400, 400)
-for i = 0 to(plot.Height - 1) do
-    for j = 0 to(plot.Width - 1) do
+let plot = new Bitmap(600, 400)
+for j = 0 to(plot.Height - 1) do
+    for i = 0 to(plot.Width - 1) do
         plot.SetPixel(i,j, Color.White)
 
-plot.SetPixel(200, 200, Color.Black)
+let toSceneCoordY y = 
+    plot.Height / 2 - int (100.0 * y)
+  
+let toPlotCoordX x = 
+      double(x - plot.Width / 2) / 100.0
+
+let toPlotCoordY y = 
+      double(plot.Height / 2 - y) / 100.0
+
+let cosinus x = 
+     //double(plot.Height / 2) - 30.0 * cos(double(x - plot.Width / 2) * 3.14 / 60.0) + 0.5
+     //toSceneCoordY(cos(toPlotCoordX(x))) 
+     cos(toPlotCoordX(x))
+
+let heartPlot x = 
+    //int (sqrt(cosinus(x)) * cosinus(200 * x) + sqrt(double(abs(x)) - 0.7) * double(4 - x * x) ** 0.01 )
+    //int (sqrt(cosinus(x)) * cosinus(200 * x))
+    //toSceneCoordY((sqrt(cosinus(x))))
+    //toSceneCoordY(sqrt(cosinus(x)) * cosinus(200 * x))
+    //toSceneCoordY(sqrt(cosinus(x)) * cosinus(200 * x) + sqrt(double(abs(toPlotCoordX(x))) - 0.7) * (4.0 - toPlotCoordX(x) ** 2.0) ** 0.01)
+    //toSceneCoordY((sqrt(abs(toPlotCoordX(x))) - 0.7) * (4.0 - toPlotCoordX(x) ** 2.0) ** 0.01)
+    toSceneCoordY(sqrt(cosinus(x)) * cosinus(200 * x) + (sqrt(abs(toPlotCoordX(x))) - 0.7) * (4.0 - toPlotCoordX(x) ** 2.0) ** 0.01)
+
+//plot.SetPixel(200, 200, Color.Black)
+for x = 0 to plot.Width - 1 do
+        for y = 0 to plot.Height - 1 do
+           if ((heartPlot(x) > 0) && (heartPlot(x) < plot.Height)) then
+                plot.SetPixel(x , heartPlot(x), Color.Red)
 
 //to display results
 let window = new Form()
@@ -19,16 +46,9 @@ window.Show()
 window
 
 do Application.Run()
-*)
 
-let cMax = complex 1.0 1.0
-let cMin = complex -1.0 -1.0
 
-let rec isInMandelbrotSet (z, c, iter, count) =
-    if (cMin < z) && (z < cMax) && (count < iter) then
-        isInMandelbrotSet ( ((z * z) + c), c, iter, (count + 1) )
-    else count
-
+(*
 let scalingFactor s = s * 3.0 / 200.0
 let offsetX = -3.0
 let offsetY = -3.0
@@ -63,3 +83,4 @@ let createImage (s, mx, my) =
     temp
 
 do Application.Run(createImage (1.0, -1.5, -1.5))
+*)
