955e3507f33fe6f55f8213223bd91480201a1ab3
DianaAnisutina <freilina_07@mail.ru>
2013-04-21 18:26:34 +0700

    cosinus can be drawed
    added cosinus drawing

--- BitmapProject/BitmapProject/drawer.fs
+++ BitmapProject/BitmapProject/drawer.fs
@@ -29,9 +29,9 @@ let rec isInMandelbrotSet (z, c, iter, count) =
         isInMandelbrotSet ( ((z * z) + c), c, iter, (count + 1) )
     else count
 
-let scalingFactor s = s * 1.0 / 200.0
-let offsetX = -1.0
-let offsetY = -1.0
+let scalingFactor s = s * 3.0 / 200.0
+let offsetX = -3.0
+let offsetY = -3.0
 
 let mapPlane (x, y, s, mx, my) =
     let fx = ((float x) * scalingFactor s) + mx
@@ -44,17 +44,22 @@ let colorize c =
     let b = (8 * c) % 255
     Color.FromArgb(r,g,b)
 
-let createImage (s, mx, my, iter) =
+let heartPlot x1 = 
+        //((cos(x) ^ 0.5) * cos(200.0 * x) + ((abs(x)) ^ 0.5 - 0.7) * ((4 - x * x) ^ 0.01)
+     //int (sin(double(x1 / 10)) * 20.0)
+     int (200.0 - 30.0 * cos(double(x1 - 200) * 3.14 / 60.0) + 0.5)
+
+let createImage (s, mx, my) =
     let image = new Bitmap(400, 400)
+    for i = 0 to(image.Height - 1) do
+        for j = 0 to(image.Width - 1) do
+            image.SetPixel(i,j, Color.White)
     for x = 0 to image.Width - 1 do
         for y = 0 to image.Height - 1 do
-            let count = isInMandelbrotSet( Complex.Zero, (mapPlane (x, y, s, mx, my)), iter, 0)
-            if count = iter then
-                image.SetPixel(x,y, Color.Black)
-            else
-                image.SetPixel(x,y, colorize( count ) )
+           if ((heartPlot(x) > 0) && (heartPlot(x) < image.Height)) then
+                image.SetPixel(x , heartPlot(x), Color.Red)            
     let temp = new Form() in
     temp.Paint.Add(fun e -> e.Graphics.DrawImage(image, 0, 0))
     temp
 
-do Application.Run(createImage (1.5, -1.5, -1.5, 20))
+do Application.Run(createImage (1.0, -1.5, -1.5))
