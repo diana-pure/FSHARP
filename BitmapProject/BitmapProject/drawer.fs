b230cac869a9c704121f2cb29d2883833f5bcd20
DianaAnisutina <freilina_07@mail.ru>
2013-04-21 19:28:44 +0700

    heart-plot is drawn with big pixels
    + a little bit clearing the project

--- BitmapProject/BitmapProject/drawer.fs
+++ BitmapProject/BitmapProject/drawer.fs
@@ -10,34 +10,37 @@ for j = 0 to(plot.Height - 1) do
     for i = 0 to(plot.Width - 1) do
         plot.SetPixel(i,j, Color.White)
 
+//converts y coordinates to the image coordinates
 let toSceneCoordY y = 
     plot.Height / 2 - int (100.0 * y)
-  
+
+//converts x coordinates to the plot coordinates  
 let toPlotCoordX x = 
       double(x - plot.Width / 2) / 100.0
 
+//converts y coordinates to the plot coordinates  
 let toPlotCoordY y = 
       double(plot.Height / 2 - y) / 100.0
 
+//cosinus: using plot's coordinates (not image's)
 let cosinus x = 
-     //double(plot.Height / 2) - 30.0 * cos(double(x - plot.Width / 2) * 3.14 / 60.0) + 0.5
-     //toSceneCoordY(cos(toPlotCoordX(x))) 
      cos(toPlotCoordX(x))
 
+//main function that defines heart-plot
 let heartPlot x = 
-    //int (sqrt(cosinus(x)) * cosinus(200 * x) + sqrt(double(abs(x)) - 0.7) * double(4 - x * x) ** 0.01 )
-    //int (sqrt(cosinus(x)) * cosinus(200 * x))
-    //toSceneCoordY((sqrt(cosinus(x))))
-    //toSceneCoordY(sqrt(cosinus(x)) * cosinus(200 * x))
-    //toSceneCoordY(sqrt(cosinus(x)) * cosinus(200 * x) + sqrt(double(abs(toPlotCoordX(x))) - 0.7) * (4.0 - toPlotCoordX(x) ** 2.0) ** 0.01)
-    //toSceneCoordY((sqrt(abs(toPlotCoordX(x))) - 0.7) * (4.0 - toPlotCoordX(x) ** 2.0) ** 0.01)
     toSceneCoordY(sqrt(cosinus(x)) * cosinus(200 * x) + (sqrt(abs(toPlotCoordX(x))) - 0.7) * (4.0 - toPlotCoordX(x) ** 2.0) ** 0.01)
 
-//plot.SetPixel(200, 200, Color.Black)
+//sat extra 8 pixels around (i, j)
+let setBigPixel x = 
+    for i = -3 to 3 do
+        for j = -3 to 3 do
+            plot.SetPixel(x + j , heartPlot(x) + i, Color.Red)
+
+//construct output image
 for x = 0 to plot.Width - 1 do
         for y = 0 to plot.Height - 1 do
            if ((heartPlot(x) > 0) && (heartPlot(x) < plot.Height)) then
-                plot.SetPixel(x , heartPlot(x), Color.Red)
+                setBigPixel(x)
 
 //to display results
 let window = new Form()
@@ -46,41 +49,3 @@ window.Show()
 window
 
 do Application.Run()
-
-
-(*
-let scalingFactor s = s * 3.0 / 200.0
-let offsetX = -3.0
-let offsetY = -3.0
-
-let mapPlane (x, y, s, mx, my) =
-    let fx = ((float x) * scalingFactor s) + mx
-    let fy = ((float y) * scalingFactor s) + my
-    complex fx fy
-
-let colorize c =
-    let r = (4 * c) % 255
-    let g = (6 * c) % 255
-    let b = (8 * c) % 255
-    Color.FromArgb(r,g,b)
-
-let heartPlot x1 = 
-        //((cos(x) ^ 0.5) * cos(200.0 * x) + ((abs(x)) ^ 0.5 - 0.7) * ((4 - x * x) ^ 0.01)
-     //int (sin(double(x1 / 10)) * 20.0)
-     int (200.0 - 30.0 * cos(double(x1 - 200) * 3.14 / 60.0) + 0.5)
-
-let createImage (s, mx, my) =
-    let image = new Bitmap(400, 400)
-    for i = 0 to(image.Height - 1) do
-        for j = 0 to(image.Width - 1) do
-            image.SetPixel(i,j, Color.White)
-    for x = 0 to image.Width - 1 do
-        for y = 0 to image.Height - 1 do
-           if ((heartPlot(x) > 0) && (heartPlot(x) < image.Height)) then
-                image.SetPixel(x , heartPlot(x), Color.Red)            
-    let temp = new Form() in
-    temp.Paint.Add(fun e -> e.Graphics.DrawImage(image, 0, 0))
-    temp
-
-do Application.Run(createImage (1.0, -1.5, -1.5))
-*)
