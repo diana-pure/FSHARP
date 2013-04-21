f13d3fa919a6862153e87fa3b5770c503c54f078
DianaAnisutina <freilina_07@mail.ru>
2013-04-21 16:49:47 +0700

    [drawer] first step
    can draw image
    fill white background
    set 1 black pixel in center
    show in windows Form

---
+++ BitmapProject/BitmapProject/drawer.fs
@@ -0,0 +1,17 @@
+﻿#light
+open Microsoft.FSharp.Math
+open System
+open System.Drawing
+open System.Windows.Forms
+
+let plot = new Bitmap(400, 400)
+for i = 0 to(plot.Height - 1) do
+    for j = 0 to(plot.Width - 1) do
+        plot.SetPixel(i,j, Color.White)
+plot.SetPixel(200, 200, Color.Black)
+let window = new Form()
+window.Paint.Add(fun e -> e.Graphics.DrawImage(plot, 0, 0))
+window.Show()
+window
+
+do Application.Run()
